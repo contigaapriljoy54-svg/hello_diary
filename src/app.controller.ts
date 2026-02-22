c01b0d1bcd9aca846754a4fb9d3d3b49741aa336
April Joy Contiga <YOUR_GITHUB_EMAIL>
2026-02-22 22:00:17 +0800

    sdsf

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,24 @@
-import { Controller, Get } from '@nestjs/common';
-import { AppService } from './app.service';
+import { Controller, Get, Res } from '@nestjs/common';
+import { join } from 'path';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
 
   @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  serveDashboard(@Res() res: any) {
+    // Serve dashboard.html
+    res.sendFile(join(process.cwd(), 'public', 'dashboard.html'));
+  }
+
+  @Get('login')
+  serveLogin(@Res() res: any) {
+    // Serve login.html
+    res.sendFile(join(process.cwd(), 'public', 'login.html'));
+  }
+
+  @Get('register')
+  serveRegister(@Res() res: any) {
+    // Serve register.html
+    res.sendFile(join(process.cwd(), 'public', 'register.html'));
   }
 }
--- src/app.module.ts
+++ src/app.module.ts
@@ -3,21 +3,22 @@ import { ConfigModule } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { UsersModule } from './users/users.module';
 import { NotesModule } from './notes/notes.module';
+import * as dotenv from 'dotenv';
+
+dotenv.config();
 
 @Module({
   imports: [
-    ConfigModule.forRoot({ isGlobal: true }),
     TypeOrmModule.forRoot({
-  type: 'mysql',
-  host: process.env.DB_HOST,
-  port: Number(process.env.DB_PORT),
-  username: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
-  autoLoadEntities: true,
-  synchronize: true,
-  logging: true,
-}),
+      type: 'mysql',
+      host: process.env.DB_HOST,
+      port: Number(process.env.DB_PORT),
+      username: process.env.DB_USER,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_NAME,
+      entities: [__dirname + '/**/*.entity{.ts,.js}'],
+      synchronize: true,
+    }),
     UsersModule,
     NotesModule,
   ],
